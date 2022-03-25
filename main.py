0e37cf0399d8679be132a559497e9007ac31c711
Kristi Achilleos <Krispy2009@gmail.com>
2022-03-25 17:17:11 +0200

    parse the sage report to make transactions dict

--- main.py
+++ main.py
@@ -1,46 +1,54 @@
 import pdb
+from pydoc import doc
 from turtle import pd
 import xml.etree.ElementTree as ET
 from openpyxl import load_workbook
 
-def build_xml():
+def build_xml(transactions):
     document = ET.Element('Document')
     ccti = ET.SubElement(document, 'CstmrCdtTrfInitn')
     grp_hdr = ET.SubElement(ccti, 'GrpHdr')
+
+    ET.dump(document)
 
 
 def parse_sage_report(filename):
     transactions = []
     wb = load_workbook(filename)
     ws = wb.active
-    import pdb; pdb.set_trace()
+    
+
+    while len(ws.merged_cells.ranges):
+        ws = unmerge_rows(ws)
+
     for xl_row in ws:
-        
         if xl_row[0].row < 8:
             continue
+
+
         transactions.append(xl_row_to_dict(xl_row))
-    print(transactions)
-            
+    return transactions
+
 def xl_row_to_dict(xl_row):
     row = {}
     row_map = {'B': 'name', 'F': 'BIC', 'H': 'IBAN', 'K': 'Currency', 'L':'Turnover', 'M': 'Balance', 'N': 'Current', 'O':'Period 1', 'P': 'Period 2', 'Q': 'Period 3', 'R':'Older'}
     for col in xl_row:
+
         if col.column_letter in row_map:
             row_title = row_map[col.column_letter]
             row[row_title] = col.value
     return row
         
 def unmerge_rows(ws):
-    for row in ws:
-        if row[0].row > 7:
-            for col in row:
-                try:
-                    ws.unmerge_cells()
-                except:
-                    pass
+    
+    for merged in ws.merged_cells:
+        ws.unmerge_cells(str(merged))
+    return ws
             
 
 
 
 if __name__ == '__main__':
-    parse_sage_report('Report.xlsx')
+    transactions = parse_sage_report('Report_small.xlsx')
+    build_xml(transactions)
+
