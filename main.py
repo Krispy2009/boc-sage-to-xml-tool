ea14ed49d7002fb68e2063ea9df53e39cddc0e8b
Kristi Achilleos <Krispy2009@gmail.com>
2022-03-25 21:00:06 +0200

    run black and edit .gitignore

--- main.py
+++ main.py
@@ -6,36 +6,35 @@ import xml.etree.ElementTree as ET
 
 from pydoc import doc
 from turtle import pd
+
 # from pprint import pprint
 from openpyxl import load_workbook
 
+
 def build_grp_hdr(ccti, number_of_txns=0, total=0):
-    grp_hdr = ET.SubElement(ccti, 'GrpHdr')
-    msg_id = ET.SubElement(grp_hdr, 'MsgId')
+    grp_hdr = ET.SubElement(ccti, "GrpHdr")
+    msg_id = ET.SubElement(grp_hdr, "MsgId")
     msg_id.text = str(uuid.uuid4())
 
-    dt = ET.SubElement(grp_hdr, 'CreDtTm')
+    # TODO: Correct format
+    dt = ET.SubElement(grp_hdr, "CreDtTm")
     dt.text = str(datetime.datetime.now())
 
-    txns = ET.SubElement(grp_hdr, 'NbOfTxs')
+    txns = ET.SubElement(grp_hdr, "NbOfTxs")
     txns.text = str(number_of_txns)
 
     # Control Sum has 2 decimal places
-    control_sum = ET.SubElement(grp_hdr, 'CtrlSum')
+    control_sum = ET.SubElement(grp_hdr, "CtrlSum")
     control_sum.text = f"{total:.2f}"
 
-    init_party = ET.SubElement(grp_hdr, 'InitgPty')
-    nm = ET.SubElement(init_party, 'Nm')
+    init_party = ET.SubElement(grp_hdr, "InitgPty")
+    nm = ET.SubElement(init_party, "Nm")
     nm.text = f"{os.environ['COMPANY_NAME']}"
 
 
-
-
-
-
 def build_xml(transactions):
-    document = ET.Element('Document')
-    ccti = ET.SubElement(document, 'CstmrCdtTrfInitn')
+    document = ET.Element("Document")
+    ccti = ET.SubElement(document, "CstmrCdtTrfInitn")
     build_grp_hdr(ccti)
     ET.dump(document)
 
@@ -44,39 +43,48 @@ def parse_sage_report(filename):
     transactions = []
     wb = load_workbook(filename)
     ws = wb.active
-    
 
+    # Keep unmerging until there are no merged cells
     while len(ws.merged_cells.ranges):
         ws = unmerge_rows(ws)
 
     for xl_row in ws:
         if xl_row[0].row < 8:
             continue
-
-
         transactions.append(xl_row_to_dict(xl_row))
     return transactions
 
+
 def xl_row_to_dict(xl_row):
     row = {}
-    row_map = {'B': 'name', 'F': 'BIC', 'H': 'IBAN', 'K': 'Currency', 'L':'Turnover', 'M': 'Balance', 'N': 'Current', 'O':'Period 1', 'P': 'Period 2', 'Q': 'Period 3', 'R':'Older'}
+    row_map = {
+        "B": "name",
+        "F": "BIC",
+        "H": "IBAN",
+        "K": "Currency",
+        "L": "Turnover",
+        "M": "Balance",
+        "N": "Current",
+        "O": "Period 1",
+        "P": "Period 2",
+        "Q": "Period 3",
+        "R": "Older",
+    }
     for col in xl_row:
 
         if col.column_letter in row_map:
             row_title = row_map[col.column_letter]
             row[row_title] = col.value
     return row
-        
+
+
 def unmerge_rows(ws):
-    
+
     for merged in ws.merged_cells:
         ws.unmerge_cells(str(merged))
     return ws
-            
 
 
-
-if __name__ == '__main__':
-    transactions = parse_sage_report('sample.xlsx')
+if __name__ == "__main__":
+    transactions = parse_sage_report("sample.xlsx")
     build_xml(transactions)
-
