680979ff19d16fcd2583b96c321eb4d0373f4808
Kristi Achilleos <Krispy2009@gmail.com>
2022-04-14 10:18:00 +0300

    add templates

--- api/handlers.py
+++ api/handlers.py
@@ -1,16 +1,8 @@
-async def root():
-    return """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
 
-            <h1>Sage report to BoC XML conversion tool</h1>
-            <form action=/>
-                <input type="file" id="myFile" name="filename">
-                <input type="submit">
-            </form>
-        </body>
-    </html>
-    """
+templates = Jinja2Templates(directory="templates")
+
+
+async def root(req: Request):
+    return templates.TemplateResponse("index.html", {"request": req})
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from boc_xml import BoCXML
 from helpers import parse_sage_report, do_calcs
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 
 from api import handlers
@@ -10,8 +10,14 @@ app = FastAPI()
 
 
 @app.get("/", response_class=HTMLResponse)
-async def root():
-    return await handlers.root()
+async def root(req: Request):
+    return await handlers.root(req)
+
+
+@app.get("/parse")
+async def parse():
+    # do the parsin'
+    return {"action": "parse"}
 
 
 if __name__ == "__main__":
