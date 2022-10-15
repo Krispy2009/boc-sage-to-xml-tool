dde283ccda1c5e068684c9e0ba367bdf7a3afd24
Kristi Achilleos <Krispy2009@gmail.com>
2022-10-15 16:46:44 +0300

    remove fast api stuff for now

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
 from boc_xml import BoCXML
 from helpers import parse_sage_report, do_calcs
 
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-
-from api import handlers
-
-app = FastAPI()
-
-
-@app.get("/", response_class=HTMLResponse)
-async def root(req: Request):
-    return await handlers.root(req)
-
-
-@app.get("/parse")
-async def parse():
-    # do the parsin'
-    return {"action": "parse"}
-
 
 if __name__ == "__main__":
-    transactions = parse_sage_report("sample.xlsx")
+    transactions = parse_sage_report("Report.xlsx")
     transactions = do_calcs(transactions)
 
     boc_xml = BoCXML(transactions)
